004d40b058c6651b138cd4cff799568c22059471
imdipo <imammuhammaddiponegoro@gmail.com>
2026-07-03 02:00:39 +0700

    main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sampling, metadata_dummy
 from pathlib import Path
 from utils.encode_stegano import main as jalankan_stegano
 from utils.databaseBuilder import XRayDatabaseBuilder
+from utils.retriever import ImageRetriever
+from utils.predict_images import prediksi_kondisi_gambar_baru
 
 folder_dataset = Path("Dataset_Paru")
 folder_metadata = Path("Metadata")
@@ -12,7 +14,7 @@ FILE_DATABASE = "pasangan_img_emb.jsonl"
 FILE_PICKLE = "pickle_PCA.pkl"
 
 
-def main():
+def proses_data_dulu():
     if not folder_dataset.is_dir():
         sampling.run_sampling(path="dataset", jumlah_gambar=50)
     
@@ -31,13 +33,40 @@ def main():
         output_pickle_path=FILE_PICKLE,
         output_jsonl_path=FILE_DATABASE
     )
-    
 
-    
+def tes_gambar_baru():
+    path_gambar = input("path gambar yang mau dicek (contoh: test.png): ").strip
+
+    if not Path(path_gambar).exists():
+        print(f"{path_gambar} ga ketemu")
+
+    retriever = ImageRetriever(FILE_PICKLE, FILE_DATABASE)
+
+    hasil_prediksi = prediksi_kondisi_gambar_baru(path_gambar, retriever)
+
+    return hasil_prediksi
+
+def main():
+    while True:
+        print("SISTEM STEGANO PARU-PARU")
+        print("1. Jalankan pipeline data (Sampling -> Metadata -> LSB). wajib kalau file pendukungnya belum ada")
+        print("2. Uji Coba Gambar Baru (Retriever)")
+        print("3. Keluar")
+        
+        pilihan = input("Pilih menu (1/2/3): ").strip()
+        
+        if pilihan == "1":
+            proses_data_dulu()
+        elif pilihan == "2":
+            tes_gambar_baru()
+        elif pilihan == "3":
+            print("ga 2 2nya dadah")
+            break
+        else:
+            print("[WARNING] Pilihan tidak valid, silakan coba lagi.")
 
 if __name__ == "__main__":
     main()
-
     
 
 
